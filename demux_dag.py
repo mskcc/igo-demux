7e69728e01a80a6c203d6e6408e29f8f118c67ad
David McManamon <dmcmanam@gmail.com>
2021-11-24 08:14:31 -0500

    added copy samplesheet bash operator

--- demux_dag.py
+++ demux_dag.py
@@ -1,7 +1,9 @@
 
 import demux
+import os
 from datetime import datetime
 
+from airflow.operators.bash import BashOperator
 from airflow.decorators import dag, task
 
 @dag(dag_id='demultiplex', schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False, tags=['demultiplex'])
@@ -27,13 +29,28 @@ def demultiplex_runs():
         Run the DRAGEN command to generate the fastq.gz files
         """
         for run_value in runs_to_demux_list:
-            print("Demultiplexing " + run_value)
-            # TODO copy sample sheet from /pskis to /home/igo/SampleSheetCopies
-            # bash_copy_samplesheet_task = BashOperator(...
-            # Split sample sheet for DLP, PED-PEG & 10X
-            # for DLP run make create-metadata-yaml ss=${SAMPLESHEET} prj=${prj} project_path=${project_path}
-            # execute DRAGEN demux command(s)
+            demux_run(run_value)
         return
+
+    def demux_run(run_path):
+        print("Demultiplexing " + run_path)
+        demux_dir = str(os.path.split(run_path).tail)
+        samplesheet = "SampleSheet_" + demux_dir + ".csv"
+        #TODO just leave these paths right here for now
+        orig_samplesheet = "/pskis34/LIMS/LIMS_SampleSheets/" + samplesheet
+        dest_samplesheet = "/home/igo/SampleSheetCopies/" + samplesheet
+        
+        cp_command = "cp {} {}".format(orig_samplesheet,dest_samplesheet)
+        print("Copying sample sheet " + cp_command)
+        run_this = BashOperator(
+            task_id='run_after_loop',
+            bash_command='echo 1',
+        )
+
+        # bash_copy_samplesheet_task = BashOperator(...
+        # Split sample sheet for DLP, PED-PEG & 10X
+        # for DLP run make create-metadata-yaml ss=${SAMPLESHEET} prj=${prj} project_path=${project_path}
+        # execute DRAGEN demux command(s)
 
     runs_to_demux = find_runs("dag_runs.conf")
     demux_runs(runs_to_demux)
