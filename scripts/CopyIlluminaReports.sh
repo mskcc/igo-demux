10e88b647d088c445bfa64658e86f4806664db18
David McManamon <dmcmanam@gmail.com>
2021-12-15 13:29:52 -0500

    simpler bash, still hard to read & maintain

--- scripts/CopyIlluminaReports.sh
+++ scripts/CopyIlluminaReports.sh
@@ -4,28 +4,21 @@ DIR=/igo/staging/FASTQ
 cd $DIR
 
 echo "Searching for recently completed demuxes in $DIR"
-FASTQ_DIRS=$(find /igo/staging/FASTQ -mindepth 1 -maxdepth 1 -cmin -123)
+FASTQ_DIRS=$(find /igo/staging/FASTQ -mindepth 1 -maxdepth 1 -cmin -4000)
 for fastq_dir in ${FASTQ_DIRS}; do
-  dirName="$(echo $fastq_dir| cut -d'/' -f 2)"
-  runLong="$(echo $fastq_dir| cut -d'_' -f 3)"
-  if [[ "$runLong" == *"-"* ]]; then  # dir names are either like PITT_0276_BH2JK3BBXY or VIC_2421_000000000-C3W3G
-    runName=$runLong
-  elif [[ -z $(echo ${dirname} | grep PEPE) ]]; then
-    # Pepe directory structure is slightly different, e.g. /igo/sequencers/pepe/output/210623_PEPE_8_AAAKYHCM5/
-    runName=${runLong:0:9}
-  else
-    runName=${runLong:1:9}
-  fi
+  echo "Processing $fastq_dir" # for example: /igo/staging/FASTQ/MICHELLE_0465_AH3GKKDSX3
+  runFullName="$(echo $fastq_dir| cut -d'/' -f 5)"
 
-  echo "For Run $runName whole name $runLong"
-  part1="/Reports/html/"
-  part2="/all/all/all/laneBarcode.html"
-  filename=$fastq_dir$part1$runName$part2
+  echo "For Run $runFullName"
+  # for bcl2fastq reports are located in /igo/staging/FASTQ/AYYAN_0105_000000000-K4R7C/Reports/html/000000000-K4R7C/all/all/all/
+  htmlName="/Reports/html/*/all/all/all/laneBarcode.html"
+  filename=$fastq_dir$htmlName
   
   homedir="/home/igo/html/"
   html="_laneBarcode.html"
   htmlnewfile="_laneBarcode_.html"
-  copiedname=$homedir$runName$html
+  copiedname=$homedir$runFullName$html
+  copiednamePlus=$homedir$runFullName$htmlnewfile
 
   reports="/Reports/"
   dragen_reports_dir=$fastq_dir$reports
@@ -33,29 +26,30 @@ for fastq_dir in ${FASTQ_DIRS}; do
   echo $dragen_replay
   
   # Check for bcl2fastq laneBarcode.html existence
+  echo "Checking for existence of $filename"
   if [ -f $filename ]; then
     echo "Copying $filename to $copiedname"
     cp -p $filename $copiedname
 
     /opt/common/CentOS_7/python/python-3.7.1/bin/python3 /igo/work/igo/igo-demux/scripts/barcodelookup.py $copiedname /igo/work/igo/igo-demux/scripts/Barcodes.json
     toDir=/srv/www/sequencing-qc/static/html/FASTQ/
-    toName=$toDir$dirName$html
-    enrichedName=$homedir$runName$htmlnewfile
+    toName=$toDir$runFullName$html
 
-    touch $enrichedName -r $filename #set correct timestamp on new html file
-    echo "scp $enrichedName to $toName"
-    scp -p $enrichedName igo@igo:$toName
+    touch $copiednamePlus -r $filename #set correct timestamp on new html file
+    echo "scp $copiednamePlus to $toName"
+    scp -p $copiednamePlus igo@igo:$toName
   elif [ -f $dragen_replay ]; then
     # This was a DRAGEN demux
     toDir=/srv/www/sequencing-qc/static/html/FASTQ/
-    toNameLocal=$homedir$dirName$html
-    toNameRemote=$toDir$dirName$html
-    python3 /igo/work/igo/igo-demux/scripts/dragen_csv_to_html.py $dragen_reports_dir $toNameLocal
+    toNameLocal=$homedir$runFullName$html
+    toNameRemote=$toDir$runFullName$html
+    echo "Converting DRAGEN reports in folder $dragen_reports_dir to name $toNameLocal"
+    python /igo/work/igo/igo-demux/scripts/dragen_csv_to_html.py $dragen_reports_dir $toNameLocal
 
     touch $toNameLocal -r $dragen_replay #set correct timestamp on new html file from a DRAGEN demux file
     echo "scp $toNameLocal to $toNameRemote"
     scp -p $toNameLocal igo@igo:$toNameRemote
   else
-    echo "Failed to copy demux reports"
+    echo "Failed to copy demux reports for run $fastq_dir"
   fi
 done
