54c133242b2aaa181919d423652446b89aede9f6
David McManamon <dmcmanam@gmail.com>
2021-12-16 21:28:23 -0500

    added barcode list for dual index sample sheets

--- SampleSheet.py
+++ SampleSheet.py
@@ -3,11 +3,15 @@ import pytest
 
 class SampleSheet:
     def __init__(self, path_to_samplesheet):
-        df = pandas.read_csv(path_to_samplesheet,skiprows=17)  # skip the header and read only data rows in the this dataframe
+        # skip the header and read only data rows in the this dataframe - the [Data] section
+        df = pandas.read_csv(path_to_samplesheet,skiprows=17)  
         # set of all recipes in the sample sheet
         self.recipe_set = set(df['Sample_Well'].tolist())  # Sample_Well column has the recipe, convert it to a set
-        # TODO for dual barcode could concat "index" and "index2" columns
-        self.barcode_list = [] # list of all barcodes in the sample sheet with dual barcodes separated by a "-"
+        # for dual barcode sample sheets concat "index" and "index2" columns
+        index_list = df['index'].tolist()
+        index2_list = df['index2'].tolist()
+        self.barcode_list = [a + b for a, b in zip(index_list, index2_list)]
+        
         # TODO 
         self.read_lengths = [] # such as 151,151 for a PE run
     
@@ -22,3 +26,8 @@ class SampleSheet:
 def test_recipe_set():
     x = SampleSheet("test/SampleSheet.csv")
     assert ("DLP" in x.recipe_set)
+
+def test_barcode_list():
+    x = SampleSheet("test/SampleSheet.csv")
+    assert ("AAGGACATAACCCCGT" in x.barcode_list)
+    
