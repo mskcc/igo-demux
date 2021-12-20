63902b3bc81187b70fab8d828fa7015a262bf1a2
Mirhaj <f.mirhaj@gmail.com>
2021-12-20 16:52:56 -0500

    Figuring out the input/outputs

--- Fingerprinting/fingerprinting_dag.py
+++ Fingerprinting/fingerprinting_dag.py
@@ -1,41 +1,56 @@
 import subprocess
+from sys import path_importer_cache, stdout
+from time import process_time
 from airflow import DAG
 from airflow.models.variable import Variable
 
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
-with DAG(
-    dag_id='fingerprinting',
-    tags=['fingerprinting'],
-) as dag:
+# with DAG(
+#     dag_id='fingerprinting',
+#     tags=['fingerprinting'],
+# ) as dag:
 
-    def fingerprint(cmo_id):
-        bams = Variable.get("Ready_to_fingerprint")
-        bam_path=subprocess._FILE()
-        vcf_output_directory=""
-        Variable.set("Ready_to_fingerprint", )
+def fingerprint(patient_id, run):
+    bams = Variable.get("Ready_to_fingerprint")
+    Variable.set("Ready_to_fingerprint", )
 
-        #find all bams
-        for bam in bams:
-            print("Running extract fingerprint: " + command1)
-            haplotype_map=
-            input_bam=
-            output_vcf=
-            reference_seq=
-            patient_id=
-            command1 = "gatk ExtractFingerprint --HAPLOTYPE_MAP \'{}\'  --INPUT \'{}\' --OUTPUT \'{}\' --REFERENCE_SEQUENCE \'{}\' --SAMPLE_ALIAS \'{}\'".format(haplotype_map, input_bam, output_vcf, reference_seq, cmo_id)
-            subprocess.run(command1, shell=True, check=True)
+    STATS_DIR = '/igo/staging/stats/'
+    REFERENCE_SEQUENCE_DIR = '/igo/work/genomes/H.sapiens/GRCh38.p13/GRCh38.p13.dna.primary.assembly.fa'
+    HAPLOTYPE_MAP = "/home/igo/fingerprint_maps/map_files/hg38_igo.map"
+    EXECUTION_DIR = STATS_DIR + run
+    t_start = process_time()
+    vcfs = []
+    # bam_dir = '/igo/staging/stats/'
+    #find all bams
+    input_bams = []
+    input_bams = subprocess.run('find . -maxdepth 2 -name *' + run + '*.bam"')
+    input_bams = input_bams.stdout.split('\n')
+    for bam in input_bams:
+        output_vcf = STATS_DIR + run + '/VCF/' + patient_id + '_' + run + '.vcf'
+        subprocess.chdir(EXECUTION_DIR)    
+        command1 = "bsub gatk ExtractFingerprint --HAPLOTYPE_MAP \'{}\'  --INPUT \'{}\' --OUTPUT \'{}\' --REFERENCE_SEQUENCE \'{}\' --SAMPLE_ALIAS \'{}\'".format(HAPLOTYPE_MAP, bam, output_vcf, REFERENCE_SEQUENCE_DIR, patient_id)
+        print("Running extract fingerprint: " + command1)
+        subprocess.run(command1, shell=True, check=True)
+        vcfs.append(output_vcf)
 
-            print("Running cross-check fingerprint: " + command2)
-            command2 = "gatk CrosscheckFingerprints LOD_THRESHOLD=-5.0 CROSSCHECK_BY=FILE NUM_THREADS=30 OUTPUT=crosscheck_fingerprint.tsv HAPLOTYPE_MAP=\'{}\' INPUT=\'{}\'".format(haplotype_map, output_vcf)
-            subprocess.run(command1, shell=True, check=True)
+    for vcf in vcfs:
+        command2 = "bsub gatk CrosscheckFingerprints LOD_THRESHOLD=-5.0 CROSSCHECK_BY=FILE NUM_THREADS=30 OUTPUT=crosscheck_fingerprint.tsv HAPLOTYPE_MAP=\'{}\' INPUT=\'{}\'".format(HAPLOTYPE_MAP, vcf)
+        print("Running cross-check fingerprint: " + command2)
+        subprocess.run(command1, shell=True, check=True)
+
+    t_stop = process_time()
+    print("Elapsed time to fingerprint: ", t_stop - t_start)
         
 
-    fingerprint=BashOperator(
-        task_id='fingerprinting',
-        bash_command=fingerprint,
-    )
-    fingerprint
+fingerprint=PythonOperator(
+    task_id='fingerprinting',
+    bash_command=fingerprint,
+)
+fingerprint
+
 
 if __name__ == "__main__":
-    dag.cli()
+    #dag.cli()
+    fingerprint()
