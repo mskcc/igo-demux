60c8034300bbc423919f2532ef19e6ddf840bd89
David McManamon <dmcmanam@gmail.com>
2022-02-02 11:56:54 -0500

    read one run to demux from file

--- find_completed_runs_dag.py
+++ find_completed_runs_dag.py
@@ -67,83 +67,80 @@ with DAG(
     tags=["find_completed_runs"],
 ) as dag:
 
-   def find_runs_completed(ds, **kwargs):
-      completed_runs_path = list()
+   completed_runs_path = list()
 
-      # Check if this was run with a config parameter specifying the run to demux
-      if "sequencer_path" in kwargs:
-         sequencer_path = kwargs["params"]["sequencer_path"]
-         completed_runs_path = [sequencer_path + "/RTAComplete.txt"]
-         print("Demuxing single run only {}".format(completed_runs_path))
-      else:
-         print("Processing sequencer list: {}".format(sequencers))
-         # should match schedule_interval above
-         time_to_search = Variable.get("completed_run_search_interval_mins", default_var=60)
-         print("Searching for runs completed in the last {} minutes".format(time_to_search))
-         completed_runs_path = sequencer.find_completed_runs(sequencers, time_to_search)
+   #sequencer_path = kwargs["params"]["sequencer_path"]
+   #print("Attempting to demux single run only from sequencer path {}".format(sequencer_path))
+   #completed_runs_path = sequencer_path + "/RTAComplete.txt"
+   demux_special = "/igo/sequencers/run_to_demux.txt"
+   if os.path.exists(demux_special):
+      run_to_demux_file = open("/igo/sequencers/run_to_demux.txt", "r")
+      demux = run_to_demux_file.readline().strip()
+      run_to_demux_file.close()
+      if len(demux) > 0:
+         completed_runs_path.append(demux + "/RTAComplete.txt")
 
-      for run_path in completed_runs_path:
-         # remove file name from path, for example: /igo/sequencers/michelle/211129_MICHELLE_0461_AHMJFJDSX2/CopyComplete.txt
-         completed_run_path = os.path.dirname(run_path)
-         print("Copying sample sheet(s) for completed run:" + completed_run_path)
-         # ex: 211129_MICHELLE_0461_AHMJFJDSX2
-         completed_run = str(os.path.basename(completed_run_path))
-         run_name_only = completed_run[7:]  # ex: MICHELLE_0461_AHMJFJDSX2
-         samplesheet = "SampleSheet_" + completed_run + ".csv"
+   if len(completed_runs_path) == 0:
+      print("Processing sequencer list: {}".format(sequencers))
+      # should match schedule_interval above
+      time_to_search = Variable.get("completed_run_search_interval_mins", default_var=60)
+      print("Searching for runs completed in the last {} minutes".format(time_to_search))
+      completed_runs_path = sequencer.find_completed_runs(sequencers, time_to_search)
 
-         orig_samplesheet_dir = Variable.get("original_samplesheet_dir", default_var="/pskis34/LIMS/LIMS_SampleSheets/")
-         orig_samplesheet = orig_samplesheet_dir + samplesheet
-         dest_samplesheet_dir = Variable.get("destination_samplesheet_dir", default_var="/igo/work/igo/SampleSheetCopies/")
-         dest_samplesheet = dest_samplesheet_dir + samplesheet
+   for run_path in completed_runs_path:
+      # remove file name from path, for example: /igo/sequencers/michelle/211129_MICHELLE_0461_AHMJFJDSX2/CopyComplete.txt
+      completed_run_path = os.path.dirname(run_path)
+      print("Copying sample sheet(s) for completed run:" + completed_run_path)
+      # ex: 211129_MICHELLE_0461_AHMJFJDSX2
+      completed_run = str(os.path.basename(completed_run_path))
+      run_name_only = completed_run[7:]  # ex: MICHELLE_0461_AHMJFJDSX2
+      samplesheet = "SampleSheet_" + completed_run + ".csv"
 
-         print("Reading the LIMS sample sheet {}".format(orig_samplesheet))
-         ss_orig = SampleSheet(orig_samplesheet)
-         ss_orig.remove_sample_prefix()
-         ss_orig.path = dest_samplesheet
+      orig_samplesheet_dir = Variable.get("original_samplesheet_dir", default_var="/pskis34/LIMS/LIMS_SampleSheets/")
+      orig_samplesheet = orig_samplesheet_dir + samplesheet
+      dest_samplesheet_dir = Variable.get("destination_samplesheet_dir", default_var="/igo/work/igo/SampleSheetCopies/")
+      dest_samplesheet = dest_samplesheet_dir + samplesheet
 
-         ss_list = ss_orig.split_sample_sheet()
+      print("Reading the LIMS sample sheet {}".format(orig_samplesheet))
+      ss_orig = SampleSheet(orig_samplesheet)
+      ss_orig.remove_sample_prefix()
+      ss_orig.path = dest_samplesheet
 
-         email_to = Variable.get("email_to", default_var="skigodata@mskcc.org")
-         send_demux_email = EmailOperator(
-             task_id='send_demux_email'+run_name_only,
-             to=email_to,
-             subject='IGO Cluster New Run Sent for Demuxing',
-             html_content="<h3>{}</h3> sent to DRAGEN split into {} sample sheets".format(run_name_only, ss_list),
-             dag=dag
-         )
+      ss_list = ss_orig.split_sample_sheet()
 
-         counter = 0
-         for samplesheet in ss_list:
-             counter = counter + 1
-             print("Saving sample sheet {}".format(samplesheet.path))
-             samplesheet.write_csv()
+      email_to = Variable.get("email_to", default_var="skigodata@mskcc.org")
+      send_demux_email = EmailOperator(
+         task_id='send_demux_email'+run_name_only,
+            to=email_to,
+            subject='IGO Cluster New Run Sent for Demuxing',
+            html_content="<h3>{}</h3> sent to DRAGEN split into {} sample sheets".format(run_name_only, ' '.join(ss_list)),
+            dag=dag
+        )
 
-             demux_dict = {}
-             demux_dict['samplesheet'] = samplesheet.path
-             demux_dict['sequencer_path'] = completed_run_path
-             demux_args_json = json.dumps(demux_dict)
+      counter = 0
+      for samplesheet in ss_list:
+         counter = counter + 1
+         print("Saving sample sheet {}".format(samplesheet.path))
+         samplesheet.write_csv()
 
-             future = '"'+(datetime.datetime.now() + datetime.timedelta(seconds=(counter*10))).strftime("%Y-%m-%dT%H:%M:%SZ")+'"'
-             # Airflow required arguments to trigger a dag - execution date and conf arguments
-             dag_json = '{"execution_date": ' + future + ',"conf": '+demux_args_json+'}'
-             print("Calling demux with execution time and args:" + dag_json)
+         demux_dict = {}
+         demux_dict['samplesheet'] = samplesheet.path
+         demux_dict['sequencer_path'] = completed_run_path
+         demux_args_json = json.dumps(demux_dict)
 
-             trigger_dag_demux = SimpleHttpOperator(
-                 task_id="demux_" +
-                 str(os.path.basename(samplesheet.path)).replace(".csv", "").replace("SampleSheet_", ""),
-                 http_conn_id='airflow-api',
-                 endpoint='api/v1/dags/demux_run/dagRuns',
-                 method='POST',
-                 headers={'Content-Type': 'application/json'},
-                 data=dag_json,
-             )
+         future = '"'+(datetime.datetime.now() + datetime.timedelta(seconds=(counter*10))).strftime("%Y-%m-%dT%H:%M:%SZ")+'"'
+         # Airflow required arguments to trigger a dag - execution date and conf arguments
+         dag_json = '{"execution_date": ' + future + ',"conf": '+demux_args_json+'}'
+         print("Calling demux with execution time and args:" + dag_json)
+
+         trigger_dag_demux = SimpleHttpOperator(
+            task_id="demux_" +
+            str(os.path.basename(samplesheet.path)).replace(".csv", "").replace("SampleSheet_", ""),
+               http_conn_id='airflow-api',
+               endpoint='api/v1/dags/demux_run/dagRuns',
+               method='POST',
+               headers={'Content-Type': 'application/json'},
+               data=dag_json,
+            )
 
          send_demux_email >> trigger_dag_demux
-   
-   find_completed = PythonOperator(
-      task_id='find_runs',
-      python_callable=find_runs_completed,
-   )
-
-   find_completed
-      
