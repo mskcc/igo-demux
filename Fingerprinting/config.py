64efa6452d03672b1da43b834cf20bf950a0fcf4
Mirhaj <f.mirhaj@gmail.com>
2022-02-08 15:14:28 -0500

    Config.

--- Fingerprinting/config.py
+++ Fingerprinting/config.py
@@ -1,4 +1,4 @@
-LIMS_USER=""
-LIMS_PASSWORD=""
-LIMS_HOST_DEV=""
-LIMS_HOST_PROD=""
+LIMS_USER="pms"
+LIMS_PASSWORD="tiagostarbuckslightbike"
+LIMS_HOST_PROD="igolims.mskcc.org:8443"
+LIMS_HOST_DEV="igo-lims04.mskcc.org:8443"
