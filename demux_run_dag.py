8f108c6e28ff9d6e29dfb20fafe65a22084971f4
David McManamon <dmcmanam@gmail.com>
2022-04-16 20:53:08 -0400

    Update demux_run_dag.py

--- demux_run_dag.py
+++ demux_run_dag.py
@@ -242,7 +242,7 @@ with DAG(
                 job_name = sequencer_and_run + "_" + sample
                 bsub = "bsub -J {} -eo /igo/staging/stats/{}/{}.out -q dragen -m id02 -n 48 -M 4 ".format(job_name, sequencer_and_run)
                 dragen_cmd_1 = "/opt/edico/bin/dragen --ref-dir /staging/ref/GRCh38_graph --enable-duplicate-marking true --enable-map-align-output true "
-                dragen_cmd_2 = "--fastq-list /igo/staging/FASTQ/{}/Reports/fastq_list.csv --output-directory /igo/staging/stats/{}/{} ".format(sequencer_and_run, sequencer_and_run)
+                dragen_cmd_2 = "--fastq-list /igo/staging/FASTQ/{}/Reports/fastq_list.csv --output-directory /igo/staging/stats/{} ".format(sequencer_and_run, sequencer_and_run)
                 dragen_cmd_3 = "--fastq-list-sample-id {} --output-file-prefix {}".format(sample, output_prefix)
                 cmd = bsub + dragen_cmd_1 + dragen_cmd_2 + dragen_cmd_3
                 cmd_set.add(cmd)
