72748e8b32705098897d222778f07c1f7bdf47b1
darrelln32 <darrelln399@gmail.com>
2022-06-21 13:45:32 -0400

    Create convert_SI_barcodes.py

---
+++ scripts/convert_SI_barcodes.py
@@ -0,0 +1,17 @@
+import csv
+import pandas as pd
+import numpy as np
+import argparse
+import os
+import glob
+from xml.etree import ElementTree
+from subprocess import call
+from sample_index import *
+
+
+def convert_SI_barcodes(sample_sheet):
+  """ function to convert SI barcodes from sample sheet to the 10X quad barcodes from the sample_index.py script """
+  
+  for
+  
+  
