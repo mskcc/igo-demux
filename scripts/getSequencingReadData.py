8b2d8af1a650cc84d487f71712c2f06b826526d3
darrelln32 <darrelln399@gmail.com>
2022-09-15 14:35:18 -0400

    Update getSequencingReadData.py
    adding another set of atac reads to the atac reads list

--- scripts/getSequencingReadData.py
+++ scripts/getSequencingReadData.py
@@ -6,31 +6,32 @@ import argparse
 from xml.etree import ElementTree
 
 
-def getSequencingReadData(sequencerPath):
+def get_sequencing_read_data(sequencer_path):
 	""" method to check the read lengths in the RunInfo.xml file """
 	
 	# compare tag
-	atacReads = [51, 8, 24, 50]
+	atac_reads = [[51, 8, 24, 50], [51, 8, 16, 50]]
+	
 	
 	# set variables to parge the XML file
-	runInfoFile = sequencerPath + '/RunInfo.xml'
-	tree = ElementTree.parse(runInfoFile)
+	run_info_file = sequencer_path + '/RunInfo.xml'
+	tree = ElementTree.parse(run_info_file)
 	root = tree.getroot()
-	readsTag = list()
+	reads_tag = list()
 	
 	# get the run data!
 	for child in root.iter('Read'):
-		readData = list()
-		readData.append(child.attrib.get('Number', None))
-		readData.append(int(child.attrib.get('NumCycles', None)))
-		readData.append(child.attrib.get('IsIndexedRead', None))
-		readsTag.append(readData)
+		read_data = list()
+		read_data.append(child.attrib.get('Number', None))
+		read_data.append(int(child.attrib.get('NumCycles', None)))
+		read_data.append(child.attrib.get('IsIndexedRead', None))
+		reads_tag.append(read_data)
 		
 	# create a list to check the run data	
-	detectedReads = [y[1] for y in readsTag]
+	detected_reads = [y[1] for y in reads_tag]
 	
 	# check the read lengths.  if they equal the atacReads list, then set atac variable to True
-	if (detectedReads == atacReads):
+	if ((detected_reads == atac_reads[0]) or (detected_reads == atac_reads[1])):
 		atac = True
 	else:
 		atac = False
@@ -38,9 +39,9 @@ def getSequencingReadData(sequencerPath):
 	return(atac)
 		
 		
-def main(sequencerPath):
+def main(sequencer_path):
 	
-	atac = getSequencingReadData(sequencerPath)
+	atac = get_sequencing_read_data(sequencer_path)
 	
 	return(atac)
 	
@@ -49,6 +50,6 @@ def main(sequencerPath):
 if __name__ == '__main__':
 	
 	# grab the sequncer run path  as an argument
-	sequencerPath = sys.argv[1]
+	sequencer_path = sys.argv[1]
 	
-	main(sequencerPath)
+	main(sequencer_path)
