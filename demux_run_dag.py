e0a89bf52ded67b8454bae95c2238b3b26f70de3
luc <cl3262@nyu.edu>
2022-09-22 09:31:09 -0400

    Update demux_run_dag.py

--- demux_run_dag.py
+++ demux_run_dag.py
@@ -148,7 +148,7 @@ with DAG(
 
         if any("10X_" in s for s in sample_sheet.recipe_set):
             # if is atac run, demux is using cellranger mkfastq
-            if scripts.get_sequencing_read_data.main(sequencer_path):
+            if scripts.get_sequencing_read_data.main(sequencer_path)[0]:
                 scripts.get_total_reads_from_demux.by_json(sequencer_and_run)
                 upload_stats_cmd = "RUNNAME={} /igo/work/igo/igo-demux/scripts/upload_stats.sh".format(sequencer_and_run)
                 subprocess.run(upload_stats_cmd, shell=True)
