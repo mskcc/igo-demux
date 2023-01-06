4aa169b1d1e9bf6d9647f288ea088de8f9f6d452
darrelln32 <darrelln399@gmail.com>
2023-01-06 15:43:02 -0500

    Update alignment_and_picard.py
    taking out id02 and id03 from DRAGEN analysis since the licenses are expired

--- scripts/alignment_and_picard.py
+++ scripts/alignment_and_picard.py
@@ -254,7 +254,7 @@ class LaunchMetrics(object):
 		fastq_list = "/igo/staging/FASTQ/{}/Reports/fastq_list.csv ".format(run)
 		
 		launch_dragen_rna = "/opt/edico/bin/dragen -f -r {} --fastq-list {} --fastq-list-sample-id {} -a {} --intermediate-results-dir /staging/temp --enable-map-align true --enable-sort=true --enable-bam-indexing true --enable-map-align-output true --output-format=BAM --enable-rna=true --enable-duplicate-marking true --enable-rna-quantification true --output-file-prefix {} --output-directory {}".format(rna_path, fastq_list, sample.sample_id, sample_parameters["GTF"], metric_file, rna_directory)
-		bsub_launch_dragen_rna = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01 id02 id03\" -q dragen -n 48 -M 4 ".format(rna_dragen_job_name_header, sample.sample_id, rna_directory) + launch_dragen_rna
+		bsub_launch_dragen_rna = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01\" -q dragen -n 48 -M 4 ".format(rna_dragen_job_name_header, sample.sample_id, rna_directory) + launch_dragen_rna
 		print(bsub_launch_dragen_rna)
 		call(bsub_launch_dragen_rna, shell = True)
 		
@@ -286,7 +286,7 @@ class LaunchMetrics(object):
 		metric_file = "{}___P{}___{}___{}".format(run, prjct, sample.sample_id, sample_parameters["GTAG"])
 		fastq_list = "/igo/staging/FASTQ/{}/Reports/fastq_list.csv ".format(run)
 		launch_dragen = "/opt/edico/bin/dragen --ref-dir {} --fastq-list {} --fastq-list-sample-id {} --intermediate-results-dir /staging/temp --output-directory {} --output-file-prefix {} --enable-duplicate-marking true".format(dragen_path, fastq_list, sample.sample_id, dragen_directory, metric_file)
-		bsub_launch_dragen = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01 id02 id03\" -q dragen -n 48 -M 4 ".format(dragen_job_name_header, sample.sample_id, dragen_directory) + launch_dragen
+		bsub_launch_dragen = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01\" -q dragen -n 48 -M 4 ".format(dragen_job_name_header, sample.sample_id, dragen_directory) + launch_dragen
 		print(bsub_launch_dragen)
 		call(bsub_launch_dragen, shell = True)
 		
