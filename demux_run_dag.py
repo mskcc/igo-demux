4febb0b2b6c0849eb4a813cd536b2e7bfd980558
darrelln32 <darrelln399@gmail.com>
2023-01-27 14:17:24 -0500

    dragen parser
    Accidentally deleted the dragenstats_csv_to_txt python script.  implemented the dragen_parse_csv_stats_hWGS python script tp replace.the GRCh38 is hardcoded for the file names. this script is specifically to go along with the DAG for demux and hWGS scripts

--- demux_run_dag.py
+++ demux_run_dag.py
@@ -301,7 +301,7 @@ with DAG(
         sequencer = sequencer_and_run.split("_")[0]
         stats_path_for_conversion = stats_path + "/"
         stats_done_dir = "/igo/stats/DONE/" + sequencer + "/"
-        cmd_conversion = "python /igo/work/igo/igo-demux/scripts/dragenstats_csv_to_txt.py {} {}".format(stats_path_for_conversion, stats_done_dir)
+        cmd_conversion = "python /igo/work/igo/igo-demux/scripts/dragen_parse_csv_stats_hWGS.py {} {}".format(stats_path_for_conversion, stats_done_dir)
         bsub_command_conversion = "bsub -J create_txt_{} -o {}create_txt.out -w \"done({}*)\" {}".format(sequencer_and_run, stats_path_for_conversion, sequencer_and_run, cmd_conversion)
         print(bsub_command_conversion)
         subprocess.run(bsub_command_conversion, shell=True)
