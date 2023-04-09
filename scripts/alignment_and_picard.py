c678a47bb3d6b36fdcc00c89aed085355dea86d5
darrelln32 <darrelln399@gmail.com>
2023-04-09 13:33:42 -0400

    Update alignment_and_picard.py
    had to remove the hardcoded gtf_file variable because that gtf file points to the human annotation file.  if there is a new human annotation file, it needs to be changed in run_param_config.  changed back to the old code so it can pick up mouse annotation file.

--- scripts/alignment_and_picard.py
+++ scripts/alignment_and_picard.py
@@ -258,8 +258,10 @@ class LaunchMetrics(object):
 		metric_file = "{}___P{}___{}___{}".format(run, prjct, sample.sample_id, sample_parameters["GTAG"])
 		fastq_list = "/igo/staging/FASTQ/{}/Reports/fastq_list.csv ".format(run)
 		
-		gtf_file = "/igo/work/genomes/H.sapiens/hg38/gencode.v40.chr_patch_hapl_scaff.annotation.gtf"
-		launch_dragen_rna = "/opt/edico/bin/dragen -f -r {} --fastq-list {} --fastq-list-sample-id {} -a {} --intermediate-results-dir /staging/temp --enable-map-align true --enable-sort true --enable-bam-indexing true --enable-map-align-output true --output-format BAM --enable-rna true --enable-duplicate-marking true --enable-rna-quantification true --output-file-prefix {} --output-directory {}".format(rna_path, fastq_list, sample.sample_id, gtf_file, metric_file, rna_directory)
+		# gtf_file = "/igo/work/genomes/H.sapiens/hg38/gencode.v40.chr_patch_hapl_scaff.annotation.gtf"
+		# having the gtf file hardcoded to human means that the mouse RNA samples will fail.  
+		
+		launch_dragen_rna = "/opt/edico/bin/dragen -f -r {} --fastq-list {} --fastq-list-sample-id {} -a {} --intermediate-results-dir /staging/temp --enable-map-align true --enable-sort true --enable-bam-indexing true --enable-map-align-output true --output-format BAM --enable-rna true --enable-duplicate-marking true --enable-rna-quantification true --output-file-prefix {} --output-directory {}".format(rna_path, fastq_list, sample.sample_id, sample_parameters["GTF"], metric_file, rna_directory)
 		bsub_launch_dragen_rna = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01 id02 id03\" -q dragen -n 48 -M 4 {3}".format(rna_dragen_job_name_header, sample.sample_id, rna_directory, launch_dragen_rna)
 		print(bsub_launch_dragen_rna)
 		call(bsub_launch_dragen_rna, shell = True)
