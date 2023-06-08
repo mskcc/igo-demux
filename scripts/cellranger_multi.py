df4185a92ca2c911494689946615a597dd72df95
luc <cl3262@nyu.edu>
2023-06-08 15:25:52 -0400

    Update cellranger_multi.py

--- scripts/cellranger_multi.py
+++ scripts/cellranger_multi.py
@@ -61,10 +61,10 @@ def ch_file_generation(project_id):
             sample_dict[sample][i] = [sample_dict[sample][i], sample_tag_dict[sample_dict[sample][i]]]
 
     # write ch config file for this project
-    file_name = "/igo/stats/Multi_config/Project_{}/Project_{}_ch.csv".format(project_id, project_id)
+    file_name = "{}Project_{}/Project_{}_ch.csv".format(CONFIG_AREA, project_id, project_id)
     if not os.path.exists(os.path.dirname(file_name)):
         os.makedirs(os.path.dirname(file_name))
-        
+
     with open(file_name,'w') as file:
         file.write("id,name,read,pattern,sequence,feature_type\n")
         for key, value in tag_seq_dict.items():
@@ -113,7 +113,8 @@ def gather_config_info(sample_dict, genome, IGO_ID):
         elif key == "ch":
             config.lirbaries[value] = [fastq_list[value], "Multiplexing Capture"]
        
-   	
+   	return config
+
 # example config Class
 # test = Multi_Config()
 # test.gene_expression["reference"] = "/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-GRCh38-2020-A"
@@ -123,3 +124,18 @@ def gather_config_info(sample_dict, genome, IGO_ID):
 # test.samples = {"test1":"B301", "test2":"B302"}
 
 # test.write_to_csv("test.csv")
+
+
+if __name__ == '__main__':
+    # input as sample dict, genome and IGO ID
+    # Usage: python cellranger_multi.py [sample_dict] [Human/Mouse] [IGO_ID]
+    sample_dict = sys.argv[1]
+    genome = sys.argv[2]
+    IGO_ID = sys.argv[3]
+    config = gather_config_info(sample_dict, genome, IGO_ID)
+    project_ID = "_".join(IGO_ID.split("IGO_")[1].split("_")[:-1])
+    file_name = "{}/Project_{}/{}.csv".format(CONFIG_AREA, project_ID, IGO_ID)
+    config.write_to_csv(file_name)
+
+    cmd = "bsub -J {}_multi -o {}_multi.out {} --id={} --csv={} --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200".format(IGO_ID, IGO_ID, config_dict["multi"]["tool"], IGO_ID, file_name)
+    print(cmd)
