25bdbe6785e9525807cf3dcac260820e2438c4e2
David McManamon <dmcmanam@gmail.com>
2023-06-28 10:36:04 -0400

    Rolling back to allow PED-PEG to run

--- scripts/organise_fastq_split_by_lane.py
+++ scripts/organise_fastq_split_by_lane.py
@@ -4,7 +4,6 @@ import glob
 import os
 import linecache
 from subprocess import call
-import pathlib
 
 """
 Immitate the bcl2fastq option which created a sub-directory per sample from the sample sheet since IGO has been delivering fastq.gz files that way for many years.
@@ -16,29 +15,32 @@ def create_fastq_folders(run_demux_dir):
 
     for project in glob.iglob("Project_*"):
         # print(project)
-        project_dir = "{}/{}".format(run_demux_dir, project)
+        project_dir = run_demux_dir + "/" +  project
         os.chdir(project_dir)
         for fastq in glob.iglob("*fastq.gz"):
-            sample_id = re.split("_S(\d+)_L00(.)_[R|I](.)_001.fastq.gz", fastq)[0]
-            print("Processing: {}".format(sample_id))
-            sample_folder = "Sample_{}".format(sample_id)
-            pathlib.Path(sample_folder).mkdir(parents = True, exist_ok = True)
-            print(sample_folder)
-            # all_fastqs_of_one_sample = sample_name + "_*" + ".fastq.gz"
-            move_fastq_file_to_sample_folder = "mv {} {}".format(fastq, sample_folder)
-            print(move_fastq_file_to_sample_folder)
-            call(move_fastq_file_to_sample_folder, shell = True)
-            # Special behavior for 08822 projects, create merged R1 fastq.gz & merged R2 fastq.gz for bwamem2 consumption
-            if "_IGO_08822" in sample_folder and not "_RNA_IGO_" in sample_folder:
-                ppg_dir = run_demux_dir +"_PPG/" + project + "/" + sample_folder + "/"
-                print("Creating PPG fastq dirs: " + ppg_dir)
-                os.makedirs(ppg_dir)
-                cat_r1_cmd = "cat " + sample_folder + "/*_R1_001.fastq.gz > " + ppg_dir + "/" + sample_id + "_S01_R1_001.fastq.gz"
-                print(cat_r1_cmd)
-                call(cat_r1_cmd, shell = True)
-                cat_r2_cmd = "cat " + sample_folder + "/*_R2_001.fastq.gz > " + ppg_dir + "/" + sample_id + "_S01_R2_001.fastq.gz"
-                print(cat_r2_cmd)
-                call(cat_r2_cmd, shell = True)
+            sample_header = "Sample_"
+            sample_name = re.search("(.+?)_IGO_(.+?)_(\d+)", fastq)[0]
+            print("Processing: " + sample_name)
+            fastq_folder = sample_header + sample_name
+            fastqs_folders = next(os.walk("."))[1]
+            if fastq_folder not in fastqs_folders:
+                os.mkdir(fastq_folder, 0o775)
+                print(fastq_folder)
+                all_fastqs_of_one_sample = sample_name + "*" + ".fastq.gz"
+                move_fastq_2_folder = "mv " + all_fastqs_of_one_sample + " " + fastq_folder
+                print(move_fastq_2_folder)
+                call(move_fastq_2_folder, shell = True)
+                # Special behavior for 08822 projects, create merged R1 fastq.gz & merged R2 fastq.gz for bwamem2 consumption
+                if "_IGO_08822" in fastq_folder and not "_RNA_IGO_" in fastq_folder:
+                    ppg_dir = run_demux_dir +"_PPG/" + project + "/" + fastq_folder + "/"
+                    print("Creating PPG fastq dirs: " + ppg_dir)
+                    os.makedirs(ppg_dir)
+                    cat_r1_cmd = "cat " + fastq_folder + "/*_R1_001.fastq.gz > " + ppg_dir + "/" + sample_name + "_S01_R1_001.fastq.gz"
+                    print(cat_r1_cmd)
+                    call(cat_r1_cmd, shell = True)
+                    cat_r2_cmd = "cat " + fastq_folder + "/*_R2_001.fastq.gz > " + ppg_dir + "/" + sample_name + "_S01_R2_001.fastq.gz"
+                    print(cat_r2_cmd)
+                    call(cat_r2_cmd, shell = True)
 
 
 # regex. regular expression
