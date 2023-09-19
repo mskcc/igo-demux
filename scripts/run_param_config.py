dd5bd05c729051dc88698e6bc1d1a8f2586a8469
darrelln32 <darrelln399@gmail.com>
2023-09-19 14:28:16 -0400

    Update run_param_config.py
    added IMPACT-Heme recipe to script.  From the sample sheet, a sample can come across as IMPACT-Heme or IMPACT_Heme_v2

--- scripts/run_param_config.py
+++ scripts/run_param_config.py
@@ -433,6 +433,12 @@ recipe_options_mapping_UNORDERED = {
 				MSKQ: "yes",
 				MD: "yes"
 		},
+		"IMPACT-Heme": {
+				BAITS: "/igo/home/igo/resources/ilist/hg38/IMPACT-Heme_v2/IMPACT-Heme_v2_BAITS.iList",
+				TARGETS: "/igo/home/igo/resources/ilist/hg38/IMPACT-Heme_v2/IMPACT-Heme_v2_TARGETS.iList",
+				MSKQ: "yes",
+				MD: "yes"
+		},
 		"IMPACT_Heme_v2": {
 				BAITS: "/igo/home/igo/resources/ilist/hg38/IMPACT-Heme_v2/IMPACT-Heme_v2_BAITS.iList",
 				TARGETS: "/igo/home/igo/resources/ilist/hg38/IMPACT-Heme_v2/IMPACT-Heme_v2_TARGETS.iList",
