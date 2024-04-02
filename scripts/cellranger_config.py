18325f9dda1a42d464f81b0f0e92754658be08f6
luc <cl3262@nyu.edu>
2024-04-02 11:09:20 -0400

    add create bam option for new version

--- scripts/cellranger_config.py
+++ scripts/cellranger_config.py
@@ -58,7 +58,7 @@ config_dict = {
 }
 
 # cellranger command line options
-OPTIONS = " --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
+OPTIONS = " --create-bam=true --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
 
 # 10X recipe list for different pipelines
 COUNT_FLAVORS = ["10X_Genomics_GeneExpression-3", "10X_Genomics_GeneExpression-5"]
