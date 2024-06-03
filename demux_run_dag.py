8407306612f2473bb12758e3034298391ded11d9
darrelln32 <darrelln399@gmail.com>
2024-06-03 13:52:23 -0400

    Update demux_run_dag.py
    updated recipes from DLP to SC_DLP

--- demux_run_dag.py
+++ demux_run_dag.py
@@ -107,7 +107,7 @@ with DAG(
     def get_dlp_chip(samplesheet, project):
         samplesheet.df_ss_data.reset_index()
         for index, row in samplesheet.df_ss_data.iterrows():
-            if row['Sample_Well'] == 'DLP' and project == row['Sample_Project']:
+            if row['Sample_Well'] == 'SC_DLP' and project == row['Sample_Project']:
                 # return chip from 071PP_DLP_UNSORTED_128624A_13_12_IGO_09443_CU_1_1_121
                 sample = row['Sample_ID']
                 return get_dlp_chip_from_sample_name(sample)
@@ -135,7 +135,7 @@ with DAG(
         if "REFERENCE" in samplesheet_path:
             return "No stats for reference "  + samplesheet_path
 
-        if "DLP" in sample_sheet.recipe_set:
+        if "SC_DLP" in sample_sheet.recipe_set:
             scripts.get_total_reads_from_demux.run_DLP(sample_sheet, sequencer_and_run)
             scripts.upload_stats.upload_stats(sequencer_and_run)
             
