b0b6dd67d7d040888de5432044a1571154a361ae
darrelln32 <darrelln399@gmail.com>
2025-03-13 15:27:26 -0400

    Update cellranger_config.py
    upgrading to Cellranger 9.0.1 per suggestion from Cassidy. 9.0.1 is better with GemX chemistry.  Also upgrading Spaceranger version to 3.1.2

--- scripts/cellranger_config.py
+++ scripts/cellranger_config.py
@@ -5,21 +5,21 @@ STATS_AREA = "/igo/staging/CELLRANGER/"
 ACCESS = 0o775
 config_dict = {
     "count": {
-        "tool": " /igo/work/nabors/tools/cellranger-8.0.0/cellranger count ",
+        "tool": " /igo/work/nabors/tools/cellranger-9.0.1/cellranger count ",
         "genome": {
             "Human": " --transcriptome=/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-GRCh38-2020-A ",
             "Mouse": " --transcriptome=/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-mm10-2020-A "
         }
     },
     "vdj": {
-        "tool": " /igo/work/nabors/tools/cellranger-8.0.0/cellranger vdj ",
+        "tool": " /igo/work/nabors/tools/cellranger-9.0.1/cellranger vdj ",
         "genome": {
             "Human": " --reference=/igo/work/genomes/10X_Genomics/VDJ/refdata-cellranger-vdj-GRCh38-alts-ensembl-7.0.0 ",
             "Mouse": " --reference=/igo/work/genomes/10X_Genomics/VDJ/refdata-cellranger-vdj-GRCm38-alts-ensembl-7.0.0 "
         }
     },
     "multi": {
-        "tool": " /igo/work/tools/cellranger-8.0.1/cellranger multi "
+        "tool": " /igo/work/tools/cellranger-9.0.1/cellranger multi "
     },
     "arc": {
         "tool": " /igo/work/nabors/tools/cellranger-arc-2.0.2/cellranger-arc count ",
@@ -29,7 +29,7 @@ config_dict = {
         }
     },
     "spaceranger": {
-        "tool": " /igo/work/nabors/tools/spaceranger-3.0.0/spaceranger count ",
+        "tool": " /igo/work/nabors/tools/spaceranger-3.1.2/spaceranger count ",
         "genome": {
             "Human": " --transcriptome=/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-GRCh38-2020-A ",
             "Mouse": " --transcriptome=/igo/work/nabors/genomes/10X_Genomics/spatial_gex/refdata-gex-mm10-2020-A "
