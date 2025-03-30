52cb01a764b0b5f898791255a3796e1ed105f3c5
darrelln32 <darrelln399@gmail.com>
2025-03-29 23:34:45 -0400

    Update LaunchMetrics.py
    putting ID01 into production since ID03 is out and ID02 is over 90% license usage.  will run WGS samples on ID01 and run RNA on ID02

--- scripts/LaunchMetrics.py
+++ scripts/LaunchMetrics.py
@@ -179,15 +179,15 @@ class LaunchMetrics(object):
 		
 		# get the correct path for the reference
 		if (sample_parameters["GTAG"] == "GRCh38") or (sample.genome == "Synthetic"):
-			dragen_path = "/igo/work/igo/dragen_hash_tables/4.2/hg38-alt_masked.cnv.graph.hla.rna-9-r3.0-1"
-			vcfFileOption = "--qc-cross-cont-vcf /opt/edico/config/sample_cross_contamination_resource_hg38.vcf.gz"
+			dragen_path = "/igo/work/igo/dragen_hash_tables/4.3.6/hg38-alt_masked.cnv.graph.hla.rna-10-r4.0-1"
+			vcfFileOption = "--qc-cross-cont-vcf /opt/dragen/4.3.6/resources/qc/sample_cross_contamination_resource_hg38.vcf.gz"
 		else:
-			dragen_path = "/igo/work/igo/dragen_hash_tables/4.2/{}".format(sample_parameters["GTAG"])
+			dragen_path = "/igo/work/igo/dragen_hash_tables/4.3.6/{}".format(sample_parameters["GTAG"])
 			vcfFileOption = ""
 			
 		metric_file_prefix = "{}___P{}___{}___{}".format(run, sample.project[8:], sample.sample_id, sample_parameters["GTAG"])
-		launch_dragen = "/opt/edico/bin/dragen --force --ref-dir {} --fastq-list {} --fastq-list-sample-id {} --intermediate-results-dir /staging/temp --output-directory {} --output-file-prefix {} {} --enable-duplicate-marking true --enable-sort true --enable-map-align true --enable-map-align-output true --output-format cram --bin_memory 70000000000 ".format(dragen_path, fastq_list, sample.sample_id, dragen_directory, sample.sample_id, vcfFileOption)
-		bsub_launch_dragen = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id02\" -q dragen -n48 -M4 {3}".format(dragen_job_name_header, sample.sample_id, dragen_directory, launch_dragen)
+		launch_dragen = "/opt/dragen/4.3.6/bin/dragen --force --ref-dir {} --fastq-list {} --fastq-list-sample-id {} --intermediate-results-dir /staging/temp --output-directory {} --output-file-prefix {} {} --enable-duplicate-marking true --enable-sort true --enable-map-align true --enable-map-align-output true --output-format cram --bin_memory 70000000000 ".format(dragen_path, fastq_list, sample.sample_id, dragen_directory, sample.sample_id, vcfFileOption)
+		bsub_launch_dragen = "bsub -J {0}{1} -o {0}{1}.out -cwd \"{2}\" -m \"id01\" -q dragen -n48 -M4 {3}".format(dragen_job_name_header, sample.sample_id, dragen_directory, launch_dragen)
 		print(bsub_launch_dragen)
 		call(bsub_launch_dragen, shell = True)
 		
