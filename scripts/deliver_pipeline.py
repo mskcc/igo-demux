ceb243f0666f0920b81eb8cd0c8c079057e1fe6f
luc <cl3262@nyu.edu>
2025-07-10 15:02:00 -0400

    pause auto deliver of TCR manifest

--- scripts/deliver_pipeline.py
+++ scripts/deliver_pipeline.py
@@ -43,16 +43,17 @@ def deliver_pipeline_output(project, pi, requestName):
     
     # TCR seq only need deliver manifest, those files located under viale lab drive
     # example file: /pskis34/LIMS/TCRseqManifest/Project_13545_TCRseq_Manifest_Beta.csv
-    elif requestName == "TCRSeq":
-        pipeline_path_prefix = "/rtssdc/mohibullahlab/LIMS/TCRseqManifest/Project_" + project + "_TCRseq"
-        TCR_delivery_folder = delivery_folder + "/Manifest"
-        if not os.path.exists(TCR_delivery_folder):
-            print("Creating pipeline delivery folder {}".format(TCR_delivery_folder))
-            os.makedirs(TCR_delivery_folder)
+    # hold automate deliver manifest for now
+    # elif requestName == "TCRSeq":
+    #     pipeline_path_prefix = "/rtssdc/mohibullahlab/LIMS/TCRseqManifest/Project_" + project + "_TCRseq"
+    #     TCR_delivery_folder = delivery_folder + "/Manifest"
+    #     if not os.path.exists(TCR_delivery_folder):
+    #         print("Creating pipeline delivery folder {}".format(TCR_delivery_folder))
+    #         os.makedirs(TCR_delivery_folder)
         
-        cmd = "cp {}* {}/".format(pipeline_path_prefix, TCR_delivery_folder)
-        print(cmd)
-        call(cmd, shell=True)
+    #     cmd = "cp {}* {}/".format(pipeline_path_prefix, TCR_delivery_folder)
+    #     print(cmd)
+    #     call(cmd, shell=True)
 
     # For all other projects, check CELLRANGER folder first then PIPELINE folder
     else:
