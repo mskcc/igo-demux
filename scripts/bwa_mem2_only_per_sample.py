191f0b1711d904d7d428847e93f7ddaf0ade065d
darrelln32 <darrelln399@gmail.com>
2025-11-10 10:28:36 -0500

    Update bwa_mem2_only_per_sample.py
    new version of this script to use the downsampled fastqs from the PPG bam

--- scripts/bwa_mem2_only_per_sample.py
+++ scripts/bwa_mem2_only_per_sample.py
@@ -1,97 +1,71 @@
-from __future__ import print_function
+#!/usr/bin/env python3
 
-import re
 import sys
 import os
 import glob
 import argparse
-
-from os.path import join
-from os.path import basename
-from os.path import abspath
-from os.path import isdir
 from subprocess import call
-
-# Define some paths and other constants up here
-
-FASTQ_REGEX = r"(([_.]R{0}[_.].+)|([_.]R{0}\.)|(_{0}\.))f(ast)?q(\.gz)?$"
-
-def format_file_name(file_name):
-	"""Return destination file name."""
-	file_name = basename(file_name)
-	for index in [1, 2]:
-		if re.search(FASTQ_REGEX.format(index), file_name):
-			suffix = "_{}.fastq".format(index)
-			letter_index_fastq = r"[_.]R{}([_.])?\.f(ast)?q".format(index)
-			number_index_fastq = r"[_.]{}([_.])?\.f(ast)?q".format(index)
-			letter_index_any_location = r"[_.]R{}[_.]".format(index)
-			file_name = re.sub(letter_index_fastq, ".fastq", file_name)
-			file_name = re.sub(number_index_fastq, ".fastq", file_name)
-			file_name = re.sub(letter_index_any_location, "_", file_name)
-			file_name = re.sub(r"[_.]f(ast)?q", suffix, file_name)
-
-	return(file_name)
-
-def symlink_input_data(outdir, sequencing_data):
-	"""Symlink fastq files using pcapcore expected naming."""
-	links_dir = join(outdir, "input_data")
-	sym_links = []
-
-	if not isdir(links_dir):
-		os.makedirs(links_dir)
-
-	for src in map(abspath, sequencing_data):
-		dst = join(links_dir, format_file_name(basename(src)))
-		sym_links.append(dst)
-
-		try:
-			os.unlink(dst)
-			os.symlink(src, dst)
-		except OSError:
-			os.symlink(src, dst)
-
-	return sym_links
 
 # ###########  WGS script to run Juan Medina's container for the BAM
 
-def submit_jobs_to_cluster(sample_directory, work_directory, new_fastqs):
+def retrieve_downsampled_fastqs(sample_id, project_id, dragen_directory):
+	"""
+	"""
+	# grab downsampled fastqs
+	downsampled_fastq_listing = "{}/{}*.fastq".format(dragen_directory, sample_id)
+	downsampled_fastqs = glob.glob(downsampled_fastq_listing)
+	downsampled_fastqs.sort()
+
+	print(downsampled_fastq_listing)
+	print(downsampled_fastqs)
 	
-	# grab sample_id and the run
-	sample_id = sample_directory.split("/")[6][7:]
-	run = sample_directory.split("/")[4]
+	# rename those fastqs we just located
+	downsampled_fastq_r1 = "{}/{}.downsample_1.fastq".format(dragen_directory, sample_id)
+	downsampled_fastq_r2 = "{}/{}.downsample_2.fastq".format(dragen_directory, sample_id)
+	print(downsampled_fastq_r1)
+	print(downsampled_fastq_r2)
 	
-	print(sample_id)
-	print(run)
+	os.rename(downsampled_fastqs[0], downsampled_fastq_r1)
+	os.rename(downsampled_fastqs[1], downsampled_fastq_r2)
+
+	output_directory = "{}/{}".format(dragen_directory[:-7], project_id)
+	pathlib.Path(output_directory).mkdir(parents = True, exist_ok = True)
+
+	return(downsampled_fastq_r1, downsampled_fastq_r2, output_directory)
+
+
+def submit_jobs_to_cluster(downsampled_fastq_r1, downsampled_fastq_r2, output_directory, dragen_job_name_header, sample_id):
+	"""
+	"""
+	run = output_directory.split("/")[5]
 	
-	generate_ppg_bam = "/igo/work/nabors/tools/bwamem2/bwa_mem2.pl -fragment 10 -reference /igo/work/nabors/tools/bwamem2/ref/gr37.fasta -threads 32 -map_threads 32 -sample {} -outdir {} {}".format(sample_id, work_directory, " ".join(new_fastqs))
-	bsub_generate_ppg_bam = "bsub -J {0}___BWA_MEM2___{1} -o {0}___BWA_MEM2___{1}.out -m \"is01 is02 is03 is04 is05 is06 is07 is08\" -n 32 -M 8 {2}".format(run, sample_id, generate_ppg_bam)
+	generate_ppg_bam = "/igo/work/nabors/tools/bwamem2/bwa_mem2.pl -fragment 10 -reference /igo/work/nabors/tools/bwamem2/ref/gr37.fasta -threads 32 -map_threads 32 -sample {} -outdir {} {} {}".format(sample_id, output_directory, downsampled_fastq_r1, downsampled_fastq_r2)
+	bsub_generate_ppg_bam = "bsub -J {0}___BWA_MEM2_for_PPG___{1} -o {0}___BWA_MEM2_for_PPG___{1}.out -n32 -M8 {2}".format(run, sample_id, generate_ppg_bam)
 	print(bsub_generate_ppg_bam)
 	call(bsub_generate_ppg_bam, shell = True)
 
+	return()
 
 
 def main(sample_directory, work_directory):
+	"""
+	"""
+	# grab downsampled fastqs
+	downsampled_fastq_r1, downsampled_fastq_r2, output_directory = retrieve_downsampled_fastqs(sample_id, project_id, dragen_directory)
 	
-	#change to sample directory
-	os.chdir(sample_directory)
-	
-	# grab the sample fastqs
-	sample_fastqs = glob.glob("*fastq.gz")
-	print(sample_fastqs)
-	new_fastqs = symlink_input_data(work_directory, sample_fastqs)
-	new_fastqs.sort()
 	# go ahead and start the biild of the PEDPEG bams
-	submit_jobs_to_cluster(sample_directory, work_directory, new_fastqs)
+	submit_jobs_to_cluster(downsampled_fastq_r1, downsampled_fastq_r2, output_directory, dragen_job_name_header, sample_id)
 	
 	
-
 ############# MAIN ROUTINE
 if __name__ == '__main__':
 	
-	sample_directory = sys.argv[1]
-	work_directory = sys.argv[2]
+	sample_id = sys.argv[1]
+	project_id = sys.argv[2]
+	dragen_directory = sys.argv[3][:-1]
+	dragen_job_name_header = sys.argv[4]
 	
-	main(sample_directory, work_directory)
+	main(sample_id, project_id, dragen_directory, dragen_job_name_header)
 		
 	
 	
